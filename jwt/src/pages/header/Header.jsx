b297413a9c4e2cc0ef6031c92486069cae2b0a56
Shivamprajapati844505 <sp1172389@gmail.com>
2025-08-02 04:05:27 +0530

    completed header

--- jwt/src/pages/header/Header.jsx
+++ jwt/src/pages/header/Header.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Container, Nav, Navbar, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
   return (
-    <Navbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg" className="py-3">
       <Container>
         <Navbar.Brand as={Link} to="/">
-          <strong>MyApp</strong>
+          <strong>Learn JWT</strong>
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="navbar-nav" />
-        <Navbar.Collapse id="navbar-nav">
-          <Nav className="ms-auto"> 
-            <Nav.Link as={Link} to="/login" className="btn btn-outline-light me-2 no-underline">
+
+        <Navbar.Collapse id="navbar-nav" className="justify-content-end">
+          <div className="d-flex gap-2">
+            <Button variant="outline-light" as={Link} to="/login" className="no-underline">
               Login
-            </Nav.Link>
-            <Nav.Link as={Link} to="/register" className="btn btn-primary no-underline">
+            </Button>
+            <Button variant="primary" as={Link} to="/register" className="no-underline">
               SignUp
-            </Nav.Link>
-          </Nav>
+            </Button>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
